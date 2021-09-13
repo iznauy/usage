2de7862759debe22e9754e69839cf376f82a4afb
Yuan Zi <zy05160516@126.com>
2021-09-13 15:43:48 +0800

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import top.iznauy.Core;
+import top.iznauy.Utils;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Core.description());
+        System.out.println(Utils.version());
+    }
+
+}
